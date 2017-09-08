c3317645dcfdd904199be57d88fbee654b466b0d
yini89 <786035415@qq.com>
2017-09-08 14:33:13 +0800

    add js code in new1

--- drag.js
+++ drag.js
@@ -6,6 +6,7 @@ function drag(id) {
     var disX = 0;
     var disY = 0;
     obj.onmousedown = function () {
-        alert(1);
+        disX = ev.pageX - obj.offsetLeft;
+        disY = ev.pageY - obj.offsetTop;
     }
 }
