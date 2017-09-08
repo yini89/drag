62a64c2e8f38d4f59d59a4bb2dfcd9c87ab9fa27
yini89 <786035415@qq.com>
2017-09-08 16:07:25 +0800

    over over 1

--- drag.js
+++ drag.js
@@ -5,7 +5,7 @@ function drag(id) {
     var obj = document.getElementById(id);
     var disX = 0;
     var disY = 0;
-    obj.onmousedown = function () {
+    obj.onmousedown = function (ev) {
         disX = ev.pageX - obj.offsetLeft;
         disY = ev.pageY - obj.offsetTop;
         document.onmousemove = function() {
